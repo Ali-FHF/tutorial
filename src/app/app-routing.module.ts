9b40b7fecc7fbb75279d1c073e333b4045d5eecb
Ali Faqih <afaqih@firsthelp.com>
2017-11-17 09:48:26 -0600

    tutorial for the team

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodosResolver } from './todos.resolver';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todos',
+    component: TodosComponent, 
+    resolve: {
+      todos: TodosResolver
+    }
+  },
+   {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [TodosResolver]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent  {
+// child component property binding via the input decorator 
+	@Input()
+	todos: Todo[]; 
+
+
+	@Output()
+	remove: EventEmitter<Todo> = new EventEmitter(); 
+
+
+	@Output()
+	toggleComplete: EventEmitter<Todo> = new EventEmitter(); 
+
+
+
+
+  constructor() { }
+
+  onToggleTodoComplete(todo: Todo) {
+
+  	this.toggleComplete.emit(todo); 
+  }
+
+
+  onRemoveTodo(todo: Todo) {
+
+  	this.remove.emit(todo); 
+  }
+
+}
